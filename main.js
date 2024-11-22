6e693fe257dfdb27f099b8a38fc252bd556be5c7
Anderson Araujo <91352623+i-am-anderson@users.noreply.github.com>
2024-11-21 21:44:50 -0300

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ document.querySelector("#app").innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Site 2 - Redeploy</h1>
+    <h1>Site 2 - Auto Redeploy</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
